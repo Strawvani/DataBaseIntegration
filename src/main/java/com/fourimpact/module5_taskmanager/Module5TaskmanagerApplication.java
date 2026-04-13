cd35b3647bb9e5d2157a89452cef069e90edf0d6
Jordan Gonzales <jordanmenddozagonzales31@gmail.com>
2026-04-13 10:16:13 +0800

    Initial Commit

---
+++ src/main/java/com/fourimpact/module5_taskmanager/Module5TaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.fourimpact.module5_taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module5TaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module5TaskmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fourimpact/module5_taskmanager/Module5TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourimpact.module5_taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module5TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
