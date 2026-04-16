5e4e4352c07a768cdb0907bbc95ed3ef29f4dbf0
Jordan Gonzales <jordanmenddozagonzales31@gmail.com>
2026-04-16 23:57:17 +0800

    Finished Module 5

--- src/main/java/com/fourimpact/module5_taskmanager/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/fourimpact/module5_taskmanager/Exception/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends RuntimeException {
+public class GlobalExceptionHandler {
     // Handles ResourceNotFoundException -- returns HTTP 404
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
